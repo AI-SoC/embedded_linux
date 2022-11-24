22816b4c42258407b337be9a8dca81d1ec9bb618
dhkim <dahye89@naver.com>
2022-11-24 01:07:51 +0000

    test commit

--- day1/01_c_basics/02_pointer/main.c
+++ day1/01_c_basics/02_pointer/main.c
@@ -3,5 +3,13 @@
 int main() {
     printf("Running...\n");
 
+    int a = 0x12345678;
+    int *pa = &a;
+    printf("a is 0x%8X st %p\n", a, pa);
+
+    unsigned char* pb = (unsigned char*)pa;
+    printf("b is at 0x%p |  0x%2X\n", pb, (*pb+0));
+
+
     return 0;
 }
--- day1/01_c_basics/04_struct/main.c
+++ day1/01_c_basics/04_struct/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 
+typedef struct{
+    unsigned char b0 : 1;
+    unsigned char b1 : 1;
+    unsigned char b2 : 1;
+    unsigned char b3 : 1;
+    unsigned char b4 : 1;
+    unsigned char b5 : 1;
+    unsigned char b6 : 1;
+    unsigned char b7 : 1;
+} bits8;
 
+typedef struct{
+    unsigned char CNFG1;
+    unsigned char CNFG2;
+    unsigned short MODE ;
+} ADC_CNFG;
+
+typedef union{
+    ADC_CNFG adc;
+    unsigned char bytes[4];
+    bits8   regs[4];
+} flag_32bits;
 
 int main() {
     printf("Running...\n");
+    printf("struct size : %d\r\n", sizeof(ADC_CNFG));
+    ADC_CNFG adc1 = {0x01, 0x80, 0xF0};
+    flag_32bits flag;
+    flag.adc = adc1;
+    printf("bytes is 0x%8X\n", flag.bytes[3]);
+    printf("struct size : %d\r\n", sizeof(ADC_CNFG));
+    flag.regs[3].b7 = 0;
+    printf("bytes is 0x%8X\n", flag.bytes[3]);
+    printf("union size : %d\r\n", sizeof(flag_32bits));
 
 }
